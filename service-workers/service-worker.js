cdf5039ad1fc59ba398c31d4e6684756b6f2e316
Chon Garcia <c.garcia.jr@gmail.com>
2020-05-17 00:07:07 -0400

    page 27

--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,2 +1,30 @@
-self.addEventListener('install', e => { console.log('Install Event: ', e); });
-self.addEventListener('activate', e => { console.log('Activate Event: ', e); });
+const version = 1;
+const cacheName = `stale- ${version}`;
+
+self.addEventListener('install', e => {
+  console.log('Install Event: ', e);
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  console.log('Activate Event: ', e);
+  if(self.clients && clients.claim)
+    clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(response => { caches.open(cacheName).then(cache => {
+      if(response.status >= 500) {
+        cache.match(event.request).then(res => {
+          return res;
+        }).catch(() => { return response; });
+      } else {
+        cache.put(event.request, response.clone());
+        return response;
+      }
+    });
+  })
+  );
+});
+
